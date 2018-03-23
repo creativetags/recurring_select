40c4f4122a2f2f30213eaeb86ae38bed89e6bab1
Felipe Moyano <gafemoyano@gmail.com>
2018-03-23 13:15:19 -0500

    remove object being passed to value comply with rails 5.2 definition

--- app/helpers/recurring_select_helper.rb
+++ app/helpers/recurring_select_helper.rb
@@ -114,7 +114,7 @@ module RecurringSelectHelper
       end
 
       def render
-        option_tags = add_options(recurring_options_for_select(value(object), @default_schedules, @options), @options, value(object))
+        option_tags = add_options(recurring_options_for_select(value, @default_schedules, @options), @options, value)
         select_content_tag(option_tags, @options, @html_options)
       end
     end
@@ -128,7 +128,7 @@ module RecurringSelectHelper
       def to_recurring_select_tag(default_schedules, options, html_options)
         html_options = recurring_select_html_options(html_options)
         add_default_name_and_id(html_options)
-        value = value(object)
+        value = value
         options = add_options(
           recurring_options_for_select(value, default_schedules, options),
           options, value
